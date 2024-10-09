595116159fc1c5af550a470227966e9a1f7dd700
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2024-10-10 01:29:14 +0530

    update yaml file

--- pkg/reader/reader.go
+++ pkg/reader/reader.go
@@ -2,6 +2,7 @@ package reader
 
 import (
 	"os"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v2"
@@ -18,6 +19,21 @@ func NewReader(filePath string) *Reader {
 	}
 }
 
+func (r *Reader) loadFileVar() error {
+	if r.Pipeline.FileVariable.Separator == "" {
+		r.Pipeline.FileVariable.Separator = "\n"
+	}
+
+	varsByte, err := os.ReadFile(r.Pipeline.FileVariable.Path)
+	if err != nil {
+		log.Error().Err(err).Msgf("Failed to load %s var from file %s", r.Pipeline.FileVariable.Name, r.Pipeline.FileVariable.Path)
+	}
+
+	r.Pipeline.FileVariable.Values = strings.Split(string(varsByte), r.Pipeline.FileVariable.Separator)
+
+	return nil
+}
+
 func (r *Reader) Load() error {
 	contents, err := os.ReadFile(r.filePath)
 	if err != nil {
@@ -27,6 +43,11 @@ func (r *Reader) Load() error {
 
 	if err := yaml.Unmarshal(contents, &r.Pipeline); err != nil {
 		log.Error().Err(err).Msgf("failed to load yaml content from file %s", r.filePath)
+		return err
+	}
+
+	if err := r.loadFileVar(); err != nil {
+		log.Error().Err(err).Msgf("failed to load vars from file %s", r.filePath)
 		return err
 	}
 
--- pkg/reader/types.go
+++ pkg/reader/types.go
@@ -1,20 +1,28 @@
 package reader
 
 type Pipeline struct {
-	Name   string             `yaml:"name"`
-	Config PipelineConfig     `yaml:"config"`
-	Vars   []PipelineVariable `yaml:"vars"`
-	Steps  []PipelineStep     `yaml:"pipeline"`
+	Name         string               `yaml:"name"`
+	Config       PipelineConfig       `yaml:"config"`
+	Vars         []PipelineVariable   `yaml:"vars"`
+	Steps        []PipelineStep       `yaml:"pipeline"`
+	FileVariable PipelineFileVariable `yaml:"file_var"`
 }
 
 type PipelineConfig struct {
-	Workers int
+	Workers int `yaml:"workers"`
 }
 
 type PipelineVariable struct {
 	Name  string `yaml:"name"`
 	Value string `yaml:"value"`
 	Type  string `yaml:"type"`
+}
+
+type PipelineFileVariable struct {
+	Name      string   `yaml:"name"`
+	Path      string   `yaml:"path"`
+	Separator string   `yaml:"separator"`
+	Values    []string `yaml:"-"`
 }
 
 type PipelineStep struct {
